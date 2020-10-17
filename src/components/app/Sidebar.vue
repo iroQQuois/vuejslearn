406702de3b61f6a9b339f151b419b31a5a91325a
Vadim <64652301+iroQQuois@users.noreply.github.com>
2020-10-18 00:30:42 +0300

    complete sidebar

--- src/components/app/Sidebar.vue
+++ src/components/app/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <ul class="sidenav app-sidenav open">
+  <ul class="sidenav app-sidenav" :class="{open: value}">
 
     <router-link
       v-for="link in links"
@@ -16,6 +16,7 @@
 
 <script>
 export default {
+  props: ['value'],
   data: () => ({
     links: [
       { title: 'Счёт', url: '/', exact: true },
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="app-main-layout">
 
-    <Navbar />
-    <Sidebar />
-    <main class="app-content">
+    <Navbar @click="isOpen=!isOpen" />
+
+    <Sidebar v-model="isOpen"/>
+
+    <main class="app-content" :class="{full: !isOpen}">
       <div class="app-page">
         <router-view />
       </div>
@@ -22,6 +24,9 @@ import Navbar from '@/components/app/Navbar'
 import Sidebar from '@/components/app/Sidebar'
 export default {
   name: 'main-layout',
+  data: () => ({
+    isOpen: true
+  }),
   components: {
     Navbar, Sidebar
   }
