1ee87c1edf07c690b50b939a6edc79a6cf19e433
Vadim <64652301+iroQQuois@users.noreply.github.com>
2020-10-17 23:11:03 +0300

    Categories and Login

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,32 @@
+
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  }
-]
-
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      meta: { layout: 'empty' },
+      component: () => import('../views/Login.vue')
+    },
+    {
+      path: '/categories',
+      name: 'categories',
+      meta: { layout: 'main' },
+      component: () => import('../views/Categories.vue')
+    }
+  ]
 })
 
 export default router
